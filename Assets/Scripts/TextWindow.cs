4619e506f49fe8b75eeed6684ab309483bc0249b
Arran Davis <arran94@gmail.com>
2023-07-22 03:02:14 +1200

    Add Example Text Window

---
+++ Assets/Scripts/TextWindow.cs
@@ -0,0 +1,67 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+[RequireComponent(typeof(RectTransform))]
+public class TextWindow : MonoBehaviour
+{
+    [SerializeField] private TextAsset filePath;
+    [SerializeField] private Vector2 fontResolution;
+    [SerializeField] private Vector2 windowSize;
+    private TextMeshProUGUI _tmpText;
+    private RectTransform _rectTransform;
+    public int FontHeight
+    {
+        get => (int)_tmpText.fontSize;
+        set => _tmpText.fontSize = value;
+    }
+
+    private int _fontWidth;
+    public int FontWidth
+    {
+        get => _fontWidth;
+        set
+        {
+            _fontWidth = value;
+            Text = Text;
+        }
+    }
+
+    public int AsciiRows
+    {
+        get => Mathf.RoundToInt(_rectTransform.rect.width / FontWidth);
+        set => _rectTransform.sizeDelta = new Vector2(FontWidth * value, _rectTransform.rect.height);
+    }
+
+    public int AsciiColumns
+    {
+        get => Mathf.RoundToInt(_rectTransform.rect.height / FontHeight / 1.15f);
+        set => _rectTransform.sizeDelta = new Vector2(_rectTransform.rect.width, value * FontHeight * 1.15f);
+    }
+    public string Text
+    {
+        get => _tmpText.GetParsedText();
+        set => _tmpText.text = $"<mspace={FontWidth}px>" + value;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _tmpText = GetComponent<TextMeshProUGUI>();
+        _rectTransform = GetComponent<RectTransform>();
+
+        FontWidth = (int)fontResolution.x;
+        FontHeight = (int)fontResolution.y;
+
+        AsciiRows = (int)windowSize.x;
+        AsciiColumns = (int)windowSize.y;
+        
+        LoadFile(filePath); //example
+    }
+
+    public void LoadFile(TextAsset file)
+    {
+        Text = file.text;
+    }
+}
