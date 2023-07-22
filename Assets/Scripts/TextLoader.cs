5803e9294bfb6b3bc7d85e7f0124f9e6e89e5e88
Arran Davis <arran94@gmail.com>
2023-07-22 20:34:52 +1200

    Initial Narration Scene

---
+++ Assets/Scripts/TextLoader.cs
@@ -0,0 +1,47 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class TextLoader : MonoBehaviour
+{
+    [SerializeField] private TextAsset filePath;
+    [SerializeField] private int fontSize;
+    [SerializeField] private int fontWidth;
+    private TextMeshProUGUI _tmpText;
+    public int FontHeight
+    {
+        get => (int)_tmpText.fontSize;
+        set => _tmpText.fontSize = value;
+    }
+
+    private int _fontWidth;
+    public int FontWidth
+    {
+        get => _fontWidth;
+        set
+        {
+            _fontWidth = value;
+            Text = Text;
+        }
+    }
+    public string Text
+    {
+        get => _tmpText.GetParsedText();
+        set => _tmpText.text = $"<mspace={FontWidth}px>\n" + value;
+    }
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _tmpText = GetComponent<TextMeshProUGUI>();
+
+        FontWidth = fontWidth;
+        FontHeight = fontSize;
+
+        LoadFile(filePath); //example
+    }
+
+    public void LoadFile(TextAsset file)
+    {
+        Text = file.text;
+    }
+}
